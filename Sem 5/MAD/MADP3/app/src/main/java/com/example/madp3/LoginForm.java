620fd4ac4df2529726919e0d354edfdd4d5abd8e
Bhavesh Patil <96092183+Uraniumking007@users.noreply.github.com>
2023-08-02 12:07:49 +0530

    Practical 3 Added

---
+++ Sem 5/MAD/MADP3/app/src/main/java/com/example/madp3/LoginForm.java
@@ -0,0 +1,62 @@
+package com.example.madp3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginForm extends AppCompatActivity {
+
+    static final String ValidUsername = "contact@bhaveshp.dev";
+    static final String ValidPassword = "testing";
+
+    public EditText username,password;
+    public Button loginBtn;
+
+    public void auth(String user, String pass){
+        if (user.isEmpty()|| pass.isEmpty()){
+            Toast.makeText(this, "Username & password are Required",Toast.LENGTH_SHORT).show();
+        }
+        if(user.equals(ValidUsername) && pass.equals(ValidPassword)){
+            Toast.makeText(this,"Login Success", Toast.LENGTH_SHORT).show();
+
+            Intent i = new Intent(LoginForm.this, MainActivity.class);
+            startActivity(i);
+            finish();
+
+        } else {
+            Toast.makeText(this,"Username or password is Incorrect", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_form);
+
+        username = findViewById(R.id.loginUsername);
+        password = findViewById(R.id.loginPassword);
+        loginBtn = findViewById(R.id.loginSubmit);
+
+
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+
+                auth(user,pass);
+
+
+            }
+        });
+
+    }
+}
