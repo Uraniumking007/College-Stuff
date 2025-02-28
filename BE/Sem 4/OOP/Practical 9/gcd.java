cf269065badc4bf52d4e53adf8ccb105e8827b8a
Bhavesh Patil <bhaveshpatil918@gmail.com>
2025-02-28 14:12:14 +0530

    Rename GCD method for clarity and add VSCode settings for Java development

--- BE/Sem 4/OOP/Practical 9/gcd.java
+++ BE/Sem 4/OOP/Practical 9/gcd.java
@@ -3,26 +3,26 @@ import java.util.Scanner;
 public class gcd {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
+
         System.out.print("Enter first integer: ");
         int num1 = input.nextInt();
         System.out.print("Enter second integer: ");
         int num2 = input.nextInt();
-        
-        System.out.println("The GCD of " + num1 + " and " + num2 + " is " + gcd(num1, num2));
+
+        System.out.println("The GCD of " + num1 + " and " + num2 + " is " + calcGCD(num1, num2));
 
     }
-    
-    public static int gcd(int num1, int num2) {
+
+    public static int calcGCD(int num1, int num2) {
         num1 = Math.abs(num1);
         num2 = Math.abs(num2);
-        
+
         while (num2 != 0) {
             int temp = num2;
             num2 = num1 % num2;
             num1 = temp;
         }
-        
+
         return num1;
     }
 }
