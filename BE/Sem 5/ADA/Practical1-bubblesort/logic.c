7ea3f89ffdd73b42c58671861ecfbb2b45c2f3f2
UraniumKing007 <bhaveshpatil918@gmail.com>
2025-07-26 23:46:34 +0530

    Refactor bubble sort implementation for correctness and clarity

--- BE/Sem 5/ADA/Practical1-bubblesort/logic.c
+++ BE/Sem 5/ADA/Practical1-bubblesort/logic.c
@@ -4,29 +4,37 @@
 int main()
 {
     int arr[] = {1, 2, 3, 4};
+    int n = sizeof(arr) / sizeof(arr[0]); // Calculate number of elements
     bool flag = false;
-    for (int i = 0; i < sizeof(arr); i++)
+
+    // Bubble sort implementation
+    for (int i = 0; i < n - 1; i++)
     {
-        for (int j = i; j < i - sizeof(arr); i++)
+        flag = false;                       // Reset flag for each pass
+        for (int j = 0; j < n - i - 1; j++) // Fixed: increment j, correct condition
         {
-            if (arr[j] > arr[i])
+            if (arr[j] > arr[j + 1]) // Compare adjacent elements
             {
                 flag = true;
-                int x = arr[i];
-                arr[i] = arr[j];
-                arr[j] = x;
+                // Swap elements
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
-            if (flag)
-            {
-                break;
-            }
+        }
+        // If no swaps occurred, array is sorted
+        if (!flag)
+        {
+            break;
         }
     }
 
-    for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+    // Print the sorted array
+    for (int i = 0; i < n; i++)
     {
         printf("%d ", arr[i]);
     }
+    printf("\n");
 
     return 0;
 }
