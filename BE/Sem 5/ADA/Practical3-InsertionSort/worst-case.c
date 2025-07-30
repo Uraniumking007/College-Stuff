88bd55418dbcad21c3e5cad72e9407686c2d9b9f
UraniumKing007 <bhaveshpatil918@gmail.com>
2025-07-30 08:01:17 +0530

    worst case

---
+++ BE/Sem 5/ADA/Practical3-InsertionSort/worst-case.c
@@ -0,0 +1,36 @@
+
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+    int n = 10000;
+    int arr[10000];
+
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = n - i;
+    }
+
+    clock_t start = clock();
+
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+
+    clock_t end = clock();
+    double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+    printf("Insertion Sort (Worst Case)\n");
+    printf("Time taken: %f seconds\n", time_taken);
+
+    return 0;
+}
