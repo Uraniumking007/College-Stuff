eaa183a2926be66506cf2bde4c0502280bb20f51
UraniumKing007 <bhaveshpatil918@gmail.com>
2025-08-13 21:40:48 +0530

    Add timing functionality to factorial implementations

--- BE/Sem 5/ADA/Practical4-Factorial/iterative.c
+++ BE/Sem 5/ADA/Practical4-Factorial/iterative.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int factorial_recursive(int n)
 {
@@ -13,9 +14,17 @@ int factorial_recursive(int n)
 int main(int argc, char const *argv[])
 {
     int n;
+    int time_taken;
     printf("Enter a positive integer: ");
     scanf("%d", &n);
+
+    clock_t start = clock();
     int result = factorial_recursive(n);
+    clock_t end = clock();
+
+    time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
+
     printf("Factorial of %d = %d\n", n, result);
+    printf("Time taken: %d seconds\n", time_taken);
     return 0;
 }
--- BE/Sem 5/ADA/Practical4-Factorial/notIterative.c
+++ BE/Sem 5/ADA/Practical4-Factorial/notIterative.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int factorial(int n)
 {
@@ -16,7 +17,11 @@ int main()
     int n;
     printf("Enter a positive integer: ");
     scanf("%d", &n);
+    clock_t start = clock();
     int result = factorial(n);
+    clock_t end = clock();
+    double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
     printf("Factorial of %d = %d\n", n, result);
+    printf("Time taken: %f seconds\n", time_taken);
     return 0;
 }
